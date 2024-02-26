29eac676549dff402d2ed9162f73edeb8f2473a5
Ravindra Valand ᴿⱽ <thekingjarvis792@gmail.com>
2024-02-26 21:55:18 +0530

    implemented authentication

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,5 +1,89 @@
+// LoginForm.js
+
+import { useState } from "react";
+import "./Login.css"; // Import the CSS file
+import VerifyOTP from "./VerifyOtp";
+
 function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [verify, setVerify] = useState(false);
+  const [otpRef, setOtpRef] = useState("");
+
+  const handleEmailChange = (e: any) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e: any) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    const userData = {
+      email: email,
+      password: password,
+    };
+    try {
+      const response = await fetch("http://localhost:3000/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(userData),
+      });
+      const data = await response.json();
+      if (response.ok) {
+        console.log("User logged in successfully", data);
+        if (data.otpRef) {
+          setVerify(true);
+          setOtpRef(data.otpRef);
+        }
+        setError("");
+      } else {
+        console.log("Failed to login user", response.statusText);
+        setError("Invalid Credentials! Please try again.");
+      }
+    } catch (error) {
+      console.log("Failed to login user", error);
+      setError("Failed to login user. Please try again later.");
+    }
+  };
+
+  return (
+    <>
+      {!verify && (
+        <div className="login-container">
+          <h1>Login</h1>
+          <form className="login-form" onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={handleEmailChange}
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={handlePasswordChange}
+                required
+              />
+            </div>
+            <button type="submit">Login</button>
+            <div>{error}</div>
+          </form>
+        </div>
+      )}
+      {verify && <VerifyOTP otpref={otpRef} />}
+    </>
+  );
 }
 
 export default Login;
---
+++ src/components/Login/VerifyOtp.tsx
@@ -0,0 +1,62 @@
+import  { useState } from "react";
+import "./VerifyOtp.css";
+
+function VerifyOtp(props: any) {
+  const [otp, setOtp] = useState("");
+  const [error, setError] = useState("");
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    const otpRef = props.otpref;
+    const userData = {
+      otp: otp,
+      otpRef: otpRef,
+    };
+    try {
+      const response = await fetch("http://localhost:3000/auth/validate-otp", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(userData),
+      });
+      console.log(userData)
+      const data = await response.json();
+      if (response.ok) {
+        console.log("OTP verified successfully", data);
+        setError("");
+
+        document.cookie = `token=${data.token}`;
+      } else {
+        console.log("Failed to verify OTP", response.statusText);
+        setError("Invalid OTP! Please try again.");
+      }
+    } catch (error) {
+      console.log("Failed to verify OTP", error);
+      setError("Failed to verify OTP. Please try again later.");
+    }
+  };
+  const handleChange = async (e: any) => {
+    e.preventDefault();
+    setOtp(e.target.value);
+  };
+
+  return (
+    <>
+      <div className="verify-otp-container">
+        <h2>Verify OTP</h2>
+        <form className="verify-otp-form" onSubmit={handleSubmit}>
+          <label htmlFor="otp">Enter OTP:</label>
+          <input
+            type="text"
+            id="otp"
+            value={otp}
+            onChange={handleChange}
+            required
+          />
+          <button type="submit">Verify</button>
+          <div>{error}</div>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default VerifyOtp;
